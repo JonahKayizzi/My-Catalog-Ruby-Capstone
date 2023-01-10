09ae48099b3abfa1e07d9c81e2c2ddaa4b247b41
rebaz36 <rebaz415@gmail.com>
2023-01-10 13:57:07 +0300

    add and initialize item class

---
+++ src/item.rb
@@ -0,0 +1,23 @@
+class Item
+  attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
+
+  def initialize(id, genre, author, source, label, publish_date)
+    @id = id
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = false
+  end
+
+  def can_be_archived?
+    (Date.today - publish_date) > 10
+  end
+
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+    end
+  end
+end
