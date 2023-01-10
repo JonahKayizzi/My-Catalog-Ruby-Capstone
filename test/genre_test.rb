45f4405110d0f5e7fcdc532dc126494ead56e09b
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-01-10 23:54:45 +0200

    Fix the linter errors

--- test/genre_test.rb
+++ test/genre_test.rb
@@ -2,22 +2,22 @@ require_relative '../src/item'
 require_relative '../src/genre'
 
 describe 'Genre class' do
-    before :each do
-        @genre = Genre.new(1, 'ali')
-    end
-    
-    it 'this should validate genre id' do
-        expect(@genre.id).to eq 1
-    end
-    it 'this should validate genre name' do
-        expect(@genre.name).to eq 'ali'
-    end
-    it 'this should validate genre to be an instent from Genre' do
-        expect(@genre).to be_an_instance_of(Genre)
-    end
-    it 'this should validate add_item method' do
-        @item = Item.new('2000-01-01', false)
-        @genre.add_item(@item)
-        expect(@genre.items.length).to eq 1
-    end
+  before :each do
+    @genre = Genre.new(1, 'ali')
+  end
+
+  it 'this should validate genre id' do
+    expect(@genre.id).to eq 1
+  end
+  it 'this should validate genre name' do
+    expect(@genre.name).to eq 'ali'
+  end
+  it 'this should validate genre to be an instent from Genre' do
+    expect(@genre).to be_an_instance_of(Genre)
+  end
+  it 'this should validate add_item method' do
+    @item = Item.new('2000-01-01', false)
+    @genre.add_item(@item)
+    expect(@genre.items.length).to eq 1
+  end
 end
