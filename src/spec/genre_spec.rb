48fb4e99f968233c5c2769e536712e3e83908a4f
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-01-11 18:38:08 +0200

    Add the add method and the list method to music

--- src/spec/genre_spec.rb
+++ src/spec/genre_spec.rb
@@ -3,14 +3,14 @@ require_relative '../Classes/genre'
 
 describe 'Genre class' do
   before :each do
-    @genre = Genre.new(1, 'ali')
+    @genre = Genre.new('soft_pop')
   end
 
   it 'this should validate genre id' do
     expect(@genre.id).to eq 1
   end
   it 'this should validate genre name' do
-    expect(@genre.name).to eq 'ali'
+    expect(@genre.name).to eq 'soft_pop'
   end
   it 'this should validate genre to be an instent from Genre' do
     expect(@genre).to be_an_instance_of(Genre)
