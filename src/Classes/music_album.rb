b3c57d61bb256ecdf8cdb50bfb4f1d916a3d76ee
rebaz36 <rebaz415@gmail.com>
2023-01-11 10:45:52 +0300

    update music album class

--- src/Classes/music_album.rb
+++ src/Classes/music_album.rb
@@ -1,16 +1,14 @@
 require_relative 'item'
 
-class Music_album < Item
-  attr_accessor :on_spotify
+class Musicalbum < Item
+  attr_accessor :on_spotify, :publish_date
 
-  def initialize(_publish_date, _archived, on_spotify)
-    # super(id, genre, author, source, label, publish_date)
-    # here I am sending this unnecessary agruments
-    super(_publish_date, _archived)
+  def initialize(on_spotify, publish_date)
+    super(publish_date)
     @on_spotify = on_spotify
   end
 
-  def can_be_archived?()
+  def can_be_archived?
     super && on_spotify
   end
 end
