dd15fba877f5ae0c56b6a02657ddfb745df7c22c
rebaz36 <rebaz415@gmail.com>
2023-01-11 10:45:34 +0300

    update item class

--- src/Classes/item.rb
+++ src/Classes/item.rb
@@ -1,29 +1,20 @@
-require_relative 'genre'
+require 'date'
 
 class Item
-  attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
+  attr_accessor :publish_date, :archived, :label, :author, :genre, :source
+  attr_reader :id
 
-  # I believe there is alot of unnecessary arguments
-  # I believe that the arguments required are (publish_date, archived, id: nil)
-
-  # I believe that the archived var should be in the init arguments
-  def initialize(publish_date, archived)
-    @id = rand(1000...1)
-    @genre = []
+  def initialize(publish_date, archived: false)
+    @id = rand(1..1000)
     @publish_date = publish_date
     @archived = archived
   end
 
   def can_be_archived?
-    # (Date.today - publish_date) > 10
-    current_year = Time.now.year
-    publish_year = Date.parse(@publish_date).year
-    current_year - publish_year > 10
+    Date.today - Date.parse(@publish_date).year > 10
   end
 
   def move_to_archive
-    return unless can_be_archived?
-
-    @archived = true
+    @archived = true if can_be_archived?
   end
 end
