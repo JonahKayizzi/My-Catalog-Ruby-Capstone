3f8eba4212448f0ab63d89ca38b5bb4c99759f65
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2023-01-11 11:55:16 +0300

    Fix linter errors

--- src/Classes/book.rb
+++ src/Classes/book.rb
@@ -4,7 +4,7 @@ class Book < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(publish_date, publisher, cover_state)
-    super(publish_date, publisher)
+    super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
