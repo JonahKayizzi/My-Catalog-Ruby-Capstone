f90d71b2a2740c4a8c46ada3fdfea94af7a5f203
rebaz36 <rebaz415@gmail.com>
2023-01-12 00:15:41 +0300

    minor fixes

--- src/SQL/schema.sql
+++ src/SQL/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE books(
-    id INT GENERATED ALWAYS AS IDENTITY,
-    publisher VARCHAR(50),
-    publish_date DATE,
-    cover_state VARCHAR(50),
+    id SERIAL PRIMARY KEY,
+    publisher VARCHAR(50) NOT NULL,
+    publish_date DATE NOT NULL,
+    cover_state VARCHAR(50) NOT NULL,
     archived BOOLEAN,
     label_id INT NOT NULL REFERENCES labels(id) ON DELETE CASCADE,
     PRIMARY KEY(id)
 );
 CREATE TABLE labels(
-    id INT GENERATED ALWAYS AS IDENTITY,
-    title VARCHAR(50),
-    color VARCHAR(50),
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    color VARCHAR(50) NOT NULL,
     PRIMARY KEY(id)
 );
