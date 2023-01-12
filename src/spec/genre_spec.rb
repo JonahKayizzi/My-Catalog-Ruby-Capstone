e4626aa7d2f25212d59c1247982ba4e317e5ce0c
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-01-12 14:28:23 +0200

    Fix bug

--- src/spec/genre_spec.rb
+++ src/spec/genre_spec.rb
@@ -6,8 +6,9 @@ describe 'Genre class' do
     @genre = Genre.new('soft_pop')
   end
 
-  it 'this should validate genre id' do
-    expect(@genre.id).to eq 1
+  it 'this should validate genre id is an integer' do
+    expect(@genre.id).to be_kind_of(Integer)
+
   end
   it 'this should validate genre name' do
     expect(@genre.name).to eq 'soft_pop'
